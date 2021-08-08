406b3e98d1721301322e0108869964c3eff4ef97
salvatierrakat <you@example.com>
2021-08-07 19:08:35 -0500

    Update

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const getMealBtn = document.getElementById('get-meal');
+const getMealBtn = document.getElementById('get_meal');
 const mealContainer = document.getElementById('meal');
 
 getMealBtn.addEventListener('click', () => {
     fetch('www.themealdb.com/api/json/v1/1/random.php')
-    .then(res => res.json())
-    .then(res => {
-    createMeal(res.meals[0]) 
-    })
+        .then(res => res.json())
+        .then(res => {
+            console.log('res')
+            createMeal(res.meals[0]) 
+         });
 });
 
-function createMeal(meal){
-    mealContainer.innerHTML = 
-}
+function createMeal(meal) {
+    mealContainer.innerHTML = `
+        <div class="row">
+            <div class"column five">
+                 <img src="${meal.strMealThumb}" alt="Meal Img" />
+            </div>
+        </div>
+      `;
+    }
